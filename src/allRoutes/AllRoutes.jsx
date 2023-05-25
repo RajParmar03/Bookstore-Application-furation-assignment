5de0edb61259fa93f482bc185e3f0a72c5f6d382
rajparmar03 <rjbusiness03@gmail.com>
2023-05-25 22:25:40 +0530

    created navbar and also started routing to different pages using react-router-dom.

--- src/allRoutes/AllRoutes.jsx
+++ src/allRoutes/AllRoutes.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
+import BookListing from '../pages/BookListing';
+import BookDetails from '../pages/BookDetails';
+import ShoppingCart from '../pages/ShoppingCart';
+import Checkout from '../pages/Checkout';
+import { Box } from '@chakra-ui/react';
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <Box>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/bookslist" element={<BookListing />}></Route>
+        <Route path="/bookdetails" element={<BookDetails />}></Route>
+        <Route path="/shoppincart" element={<ShoppingCart />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+      </Routes>
+    </Box>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Box className={Styles.navbar}>
+      <Link to="/">Home</Link>
+      <Link to="/bookslist">Books List</Link>
+      <Link to="/bookdetails">Book Details</Link>
+      <Link to="/shoppincart">Shopping Cart</Link>
+      <Link to="/checkout">Checkout</Link>
+    </Box>
   )
 }
 
